84e087416c922b2ad0a17076c08314a0aeaee511
muxia <1120236073@qq.com>
2019-12-06 01:46:56 +0800

    course

---
+++ TextGit/src/domain/course.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class course {
+	String cno;
+	String cname;
+	public String getCno() {
+		return cno;
+	}
+	public void setCno(String cno) {
+		this.cno = cno;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+}
